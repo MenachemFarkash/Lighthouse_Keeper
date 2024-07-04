9735357df828f1e9cb418ad685489d97606c8da5
Menachem Farkash <menachem.farkash1@gmail.com>
2024-07-04 12:40:58 +0300

    A little system to control the dirtiness of the glass
    control the alpha of the glass by pressing the G button.

---
+++ Assets/Scripts/Test Scripts/ChangeAlpha.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ChangeAlpha : MonoBehaviour {
+    Color color;
+    MeshRenderer meshRenderer;
+    public float alpha;
+    // Start is called before the first frame update
+    void Start() {
+        alpha = 0;
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        color = meshRenderer.material.color;
+        meshRenderer.material.color = new Color(color.r, color.g, color.b, alpha);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.G)) {
+            if (alpha < 1f)
+                alpha += 0.1f;
+            meshRenderer.material.color = new Color(color.r, color.g, color.b, alpha);
+        }
+    }
+}
